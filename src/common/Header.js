03d319d345a87f5decc1e96b5cf76641a39c8d5c
Siva <sivansbhfe@gmail.com>
2019-11-16 17:28:30 +0530

    Installed Material UI. Redirected index to login page

---
+++ src/common/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+const Header = function(props){
+   // const headerStyle={textAlign: 'center',padding: 20,background: '#000',color: '#fff',textTransform: 'uppercase'};
+    return (<div className='header'>
+    {props.heading}
+</div>);
+}
+
+export default Header;
--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import './Login.css';
-
+import Header from '../../common/Header';
 class Login extends Component {
 	render(){
-		return(<div><h1>React with Instagram API</h1></div>)
+		return(<div><Header heading="Image Viewer" /></div>)
 	}
 }
 export default Login;
