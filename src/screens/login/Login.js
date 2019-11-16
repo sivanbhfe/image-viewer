c476a40951a0bc9cf9ae5814b613869adc654e95
Siva <sivansbhfe@gmail.com>
2019-11-16 09:17:14 +0530

    Cleaned up folder and set up login js

---
+++ src/screens/login/Login.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './Login.css';
+
+class Login extends Component {
+	render(){
+		return(<div><h1>React with Instagram API</h1></div>)
+	}
+}
+export default Login;
+
