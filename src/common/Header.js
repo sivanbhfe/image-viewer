84825b5151a5cd9b93ce109765331f7af5862a97
Siva <sivansbhfe@gmail.com>
2019-11-23 13:41:59 +0530

    Commit void only formatting changes

--- src/common/Header.js
+++ src/common/Header.js
@@ -6,18 +6,18 @@ import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
 
 const Header = function(props){
-      return (<div className='header'>
-      <span className="header-text">{props.heading}</span>
-      <span>{props.accc }</span>
-      <span>{props.loggedIn }</span>
-      <div className={props.searchDisplay}><span className={props.noSearchBox}>{props.searchDisplay}</span></div>
-      <div className={props.iconDisplay}>
-        <div className="image">
-      <Link to={{pathname: '/profile/',state:{accessToken:props.accc,loggedIn:props.loggedIn}}}>
-      <img src={logo} alt="profile-icon"/>{props.logo}
-      </Link>
-      </div>
+    return (<div className='header'>
+    <span className="header-text">{props.heading}</span>
+    <span>{props.accc }</span>
+    <span>{props.loggedIn }</span>
+    <div className={props.searchDisplay}><span className={props.noSearchBox}>{props.searchDisplay}</span></div>
+    <div className={props.iconDisplay}>
+      <div className="image">
+    <Link to={{pathname: '/profile/',state:{accessToken:props.accc,loggedIn:props.loggedIn}}}>
+    <img src={logo} alt="profile-icon"/>{props.logo}
+    </Link>
     </div>
-    </div>);   
+  </div>
+  </div>);   
 }
 export default Header;
