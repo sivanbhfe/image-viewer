02e3dd331d54f8763c9ec0f74eaf2f2163b79224
Siva <sivansbhfe@gmail.com>
2019-11-24 09:45:45 +0530

    Converted Header from function to class

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,23 +1,107 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Header.css';
 import logo from '../assets/logo.png';
-
-
 import { Link } from 'react-router-dom';
 
-const Header = function(props){
-    return (<div className='header'>
-    <span className="header-text">{props.heading}</span>
-    <span>{props.accc }</span>
-    <span>{props.loggedIn }</span>
-    <div className={props.searchDisplay}><span className={props.noSearchBox}>{props.searchDisplay}</span></div>
-    <div className={props.iconDisplay}>
-      <div className="image">
-    <Link to={{pathname: '/profile/',state:{accessToken:props.accc,loggedIn:props.loggedIn}}}>
-    <img src={logo} alt="profile-icon"/>{props.logo}
-    </Link>
-    </div>
-  </div>
-  </div>);   
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
+
+const styles = theme => ({
+grow: {
+flexGrow: 1,
+},
+title: {
+color: 'initial',
+},
+bigAvatar: {
+margin: 10,
+width: 60,
+height: 60,
+},
+search: {
+position: 'relative',
+borderRadius: '4px',
+backgroundColor: '#c0c0c0',
+marginLeft: '70%',
+width: '300px',
+verticalAlign: 'center',
+},
+searchIcon: {
+height: '100%',
+color: 'black',
+position: 'absolute',
+pointerEvents: 'none',
+display: 'flex',
+alignItems: 'center',
+justifyContent: 'center',
+},
+inputRoot: {
+color: 'inherit',
+},
+inputInput: {
+transition: theme.transitions.create('width'),
+width: '100%',
+[theme.breakpoints.up('md')]: {
+width: 200,
+},
+},
+
+});
+
+class Header extends Component {
+
+constructor() {
+super();
+this.state = {
+menuIsOpen: false,
+ownerInfo: [],
+loggedIn: sessionStorage.getItem("access-token") == null ? false : true
+}
+this.baseUrl = "https://api.instagram.com/v1/users/self/?access_token=8661035776.d0fcd39.39f63ab2f88d4f9c92b0862729ee2784";
+}
+
+componentWillMount() {
+
+// Get owner info after authenticating the  accessToken generated 
+let ownerData = null;
+let xhr = new XMLHttpRequest();
+let that = this;
+xhr.addEventListener("readystatechange", function () {
+if (this.readyState === 4) {
+that.setState({
+ownerInfo: JSON.parse(this.responseText).data
+
+});
+
+}
+})
+xhr.open("GET", this.baseUrl);
+xhr.send(ownerData);
+}
+
+render(){
+return (<div className='header'>
+<span className="header-text">{this.props.heading}</span>
+
+<div className={this.props.searchDisplay}>
+<span className={this.props.noSearchBox}>
+<span className="searchcIcon">
+<IconButton className="iconButton" >
+            <SearchIcon />
+          </IconButton>
+</span>
+<InputBase placeholder="Search…"/>          
+</span>
+      </div>
+<div className={this.props.iconDisplay}>
+<div className="image">
+<Link to={{pathname: '/profile/',state:{accessToken:this.props.accc,loggedIn:this.props.loggedIn}}}>
+<img src={this.state.ownerInfo.profile_picture} alt="profile-icon"/>
+</Link>
+</div>
+</div>
+</div>);  
+}
 }
 export default Header;
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -11,6 +11,36 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+const styles = theme => ({
+    root: {
+        width: '100%',
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    card: {
+        maxWidth: '100%',
+        margin: '8px',
+        shadow: '20px',
+    },
+    bigAvatar: {
+        margin: 10,
+        width: 60,
+        height: 60,
+    },
+    button: {
+        margin: theme.spacing.unit,
+    },
+    input: {
+        display: 'none',
+    },
+    gridListMain: {
+        transform: 'translateZ(0)',
+        cursor: 'pointer',
+        
+        },          
+});
+
 class Home extends Component {
 
 constructor() {
@@ -20,8 +50,10 @@ userphotos: [],
 access_token:sessionStorage.getItem("access-token"),
 loggedIn:'false',
 hasError:false,
-accessToken:''
+accessToken:'',
+
 }
+
 }
 
 componentWillMount() {
@@ -32,6 +64,7 @@ let that = this;
 let access_token = this.state.access_token;
 let accessToken = this.state.accessToken;
 let loggedIn = false;
+
 
 // Redirecting to login page if not logged in    
 try{
@@ -46,13 +79,13 @@ if(access_token===this.state.accessToken && loggedIn===true){
 that.state.loggedIn='true';
 xhr.addEventListener("readystatechange", function () {
 if (this.readyState === 4) {
+    
 that.setState({
-userphotos: JSON.parse(this.responseText).data
+userphotos: JSON.parse(this.responseText).data,
 });
+
 }
 });
-
-
 xhr.open("GET", baseUrl+access_token);
 xhr.setRequestHeader("Cache-Control", "no-cache");
 xhr.send(data);
@@ -65,16 +98,8 @@ this.props.history.push({pathname:'/'});
 
 render(){
 return(<div>
-<div><Header heading="Image Viewer" accc={this.state.access_token} loggedIn={this.state.loggedIn} searchDisplay="dispSearch" iconDisplay="dispBlock"/></div>
-<div className="try">
-{this.state.userphotos.map(photo=>
-<Card key={"grid" + photo.id}>
-    <CardContent>
-        <img className="user-images" src={photo.images.standard_resolution.url}/>
-    </CardContent>
-</Card>
-)}
-</div>
+<div><Header heading="Image Viewer"  accc={this.state.access_token} loggedIn={this.state.loggedIn} noSearchBox="box" searchDisplay="dispSearch" iconDisplay="dispBlock"/></div>
+{this.state.userphotos.map(profile=>(<span key={"grid" + profile.id}><p><img src={profile.images.low_resolution.url}></img></p></span>))}
 </div>) 
 }
 }
